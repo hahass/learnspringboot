361a5eae97928152533e8976ca43e2f134d61a53
gengdeyang <gengdeyang@shein.com>
2019-01-12 21:00:42 +0800

    first commit

---
+++ src/main/java/com/gdy/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.gdy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public Object hello() {
+        return "hello world";
+    }
+}
---
+++ src/main/java/com/gdy/servlet/MyServlet.java
@@ -0,0 +1,15 @@
+package com.gdy.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+
+@WebServlet
+public class MyServlet extends HttpServlet {
+
+
+    public static void main(String[] args) {
+        ArrayList as = new ArrayList();
+
+    }
+}
